ab24fc141830dbaaf63c27c3a991d8f95642fcbe
McArroy <rayhanpradipta2021@gmail.com>
2024-11-13 01:47:44 +0700

    Added admin's authentication and admin's database

--- admin/app/Models/User.php
+++ admin/app/Models/User.php
@@ -12,56 +12,63 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
+	use HasApiTokens;
 
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory;
-    use HasProfilePhoto;
-    use Notifiable;
-    use TwoFactorAuthenticatable;
+	/** @use HasFactory<\Database\Factories\UserFactory> */
+	use HasFactory;
+	use HasProfilePhoto;
+	use Notifiable;
+	use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable =
+	[
+		"name",
+		// "email",
+		"password"
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'two_factor_recovery_codes',
-        'two_factor_secret',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden =
+	[
+		"password",
+		"remember_token",
+		// "two_factor_recovery_codes",
+		// "two_factor_secret"
+	];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
-    protected $appends = [
-        'profile_photo_url',
-    ];
+	/**
+	 * The accessors to append to the model's array form.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $appends =
+	[
+		"profile_photo_url"
+	];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+	/**
+	 * Get the attributes that should be cast.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function casts() : array
+	{
+		return
+		[
+			// "email_verified_at" => "datetime",
+			"password" => "hashed"
+		];
+	}
+
+	// Disable the automatic timestamps management
+    public $timestamps = false;
 }
