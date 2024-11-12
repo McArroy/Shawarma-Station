8f56175c0c4a7c0aeb028d7c237331c96a0466de
McArroy <rayhanpradipta2021@gmail.com>
2024-11-13 01:48:18 +0700

    Added admin-panel and routes

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -2,16 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function()
+{
+	return redirect()->route("adminpanel");
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(
+[
+	"auth:sanctum",
+	config("jetstream.auth_session"),
+	"verified"
+])->group(function()
+{
+	Route::get("/", function()
+	{
+		return redirect()->route("adminpanel");
+	})->name("home");
+
+	Route::get("/dashboard", function()
+	{
+		return redirect()->route("adminpanel");
+	})->name("dashboard");
+
+	Route::get("/adminpanel", function()
+	{
+		return view("admin.panel");
+	})->name("adminpanel");
 });
